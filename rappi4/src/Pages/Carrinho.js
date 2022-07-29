263bb2216cbd844e928d559b70c345767db54956
Priscilla Lucena <prilucenaoliveira@gmail.com>
2022-07-28 21:02:54 -0300

    pop up abre, mas ainda não fecha

--- rappi4/src/Pages/Carrinho.js
+++ rappi4/src/Pages/Carrinho.js
@@ -7,6 +7,7 @@ import { GlobalContext } from "../Global/GlobalContext";
 import { useParams, useNavigate } from "react-router-dom";
 import { useRequestData2 } from "../Hooks/useRequestData";
 import PopUp2 from "../Components/PopUpCarrinho/PopUpCarrinho"
+import Feed from "./Feed"
 
 
 export const Carrinho = () => {
@@ -19,7 +20,7 @@ export const Carrinho = () => {
   const [erroPost, setErroPost] = useState('');
   const [pagamento, setPagamento] = useState([]);
   const [tempoPopUp2, setTempoPopUp2] = useState(false);
-  const [cont, setCont] = useState(false);
+  // const [cont, setCont] = useState(false); retirar
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
   const [restaurante] = useRequestData2(`${BASE_URL}/restaurants/${id}`)
@@ -28,14 +29,14 @@ export const Carrinho = () => {
   const somaCarrinho = carrinho.map(item => item.precoTotalItem).reduce((prev, curr) => prev + curr, 0);
   const subTotal = (somaCarrinho + rest.shipping).toFixed(2);
 
-  
+
   const setaTempo = () => {
-    setTempoPopUp2(!tempoPopUp2);
-    window.setTimeout(() => {
-      setTempoPopUp2(!tempoPopUp2)
+    console.log("comecei")
+    setTempoPopUp2(!tempoPopUp2)
+    setTimeout(() => {
       console.log("fechei")
-      console.log(tempoPopUp2)
     }, (rest.deliveryTime * 1000))
+    setTempoPopUp2(!tempoPopUp2)
   }
 
   const enviaPedido = () => {
@@ -50,9 +51,9 @@ export const Carrinho = () => {
     //     contentType: "application/json"
     //   }
     // }).then((response) => {
-      setCont(true);
-      setaTempo();
-      setLoadingPost(false);
+
+    setaTempo();
+    setLoadingPost(false);
     // }).catch(error => {
     //   setLoadingPost(false)
     //   setErroPost(error.response)
@@ -68,16 +69,18 @@ export const Carrinho = () => {
   //   }, (rest.deliveryTime * 1000))
   // },[cont, loadingPost])
 
+  const PopUpCarrinho = () => { }
+
   const listaCarrinho = carrinho && carrinho.map((prato) => {
     return <div key={prato.id}>
       <p>quantidade {prato.quantity}</p>
       <img src={prato.photoUrl} width="75px" height="75px"></img>
       <p>{prato.name}</p>
       <p>{prato.description}</p>
-      <p>{parseFloat(prato.precoTotalItem).toFixed(2)}</p>
+      <p>{`R$: ${parseFloat(prato.precoTotalItem).toFixed(2)}`}</p>
     </div>
   });
-  
+
   return (
     <div>
       <h1>Carrinho</h1>
@@ -90,7 +93,7 @@ export const Carrinho = () => {
       <br></br>
 
       {carrinho.length === 0 && <h3>Carrinho vazio</h3>}
-      {carrinho.length === 0 ? "" :
+      {carrinho.length > 0 &&
         <div>
           <h4>{rest.name}</h4>
           <p>{rest.address}</p>
@@ -102,8 +105,8 @@ export const Carrinho = () => {
       </span>
 
       <div>{carrinho.length === 0 ? "" :
-        <h3> {`Frete:R$ ${parseFloat(rest.shipping).toFixed(2)}`} </h3>}
-        {carrinho.length === 0 ? <h3>SUBTOTAL 0,00</h3> : <h3>{`SUBTOTAL ${subTotal}`}</h3>}
+        <h3> {`Frete R$: ${parseFloat(rest.shipping).toFixed(2)}`} </h3>}
+        {carrinho.length === 0 ? <h3>SUBTOTAL 0,00</h3> : <h3>{`SUBTOTAL R$: ${subTotal}`}</h3>}
         <h5>FORMA DE PAGAMENTO</h5>
         <input type="radio" name="pagamento" value={pagamento} onChange={() => setPagamento("money")} />Dinheiro
         <input type="radio" name="pagamento" value={pagamento} onChange={() => setPagamento("creditcard")} />Cartão de Crédito
@@ -112,13 +115,15 @@ export const Carrinho = () => {
 
       {loadingPost && loadingPost && <p>Carregando...</p>}
       {!loadingPost && erroPost && <p>Ja existe um pedido em andamento!</p>}
-      {tempoPopUp2 ? <PopUp2 trigger={tempoPopUp2} setTrigger={setTempoPopUp2}>
-        <div>
-        <p>Pedido em Andamento</p>
-        <p>{rest.name}</p>
-        <p>{`Subtotal R$${subTotal}`}</p>
-        </div>
-      </PopUp2> : ""}
+      {tempoPopUp2 &&
+       (<PopUp2 trigger={tempoPopUp2} setTrigger={setTempoPopUp2}>
+       <div>
+         <p>Pedido em Andamento</p>
+         <p>{rest.name}</p>
+         <p>{`Subtotal R$ ${subTotal}`}</p>
+       </div>
+     </PopUp2>)}
+
 
 
     </div>
